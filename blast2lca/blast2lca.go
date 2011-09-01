f0ca244ede4473b7f4fc42c3d5508393bb7b7900
Miguel Pignatelli <emepyc@gmail.com>
2011-09-01 12:50:07 +0100

    Development version (0.2). Added functional annotations Most of the code has been rewritten

---
+++ blast2lca/blast2lca.go
@@ -0,0 +1,202 @@
+package main
+
+import (
+	"Blast2lca/blastm8"
+	"Blast2lca/taxonomy"
+	"Blast2lca/kegg"
+	"fmt"
+	"os"
+	"bufio"
+	"io"
+	"runtime"
+	"flag"
+	"sync"
+	"bytes"
+	"time"
+)
+
+const VERSION = 0.2
+
+const (
+	lcalim       = 0.1
+	blim         = 1000
+	bscLimFactor = 0.9
+)
+
+var (
+	cpuflag                                                                           int
+	dictflag, nodesflag, namesflag, blastfile, taxlevel, gi2kegg, binkegg, bintaxflag string
+	savememflag, verflag, helpflag, keggflag, taxIsBin, keggIsBin                     bool // HINT: keggflag is not set by the user, will be true if gi2kegg and kegg2pw are defined / The same with taxIsBin
+	printfLock                                                                        sync.Mutex
+)
+
+// HINT : Use this function for sensible output! it is threadsafe
+func printf(format string, args ...interface{}) {
+	printfLock.Lock()
+	fmt.Printf(format, args...)
+	printfLock.Unlock()
+}
+
+func init() {
+	flag.IntVar(&cpuflag, "ncpus", 4, "Number of cpus for multithreading [optional]")
+	flag.StringVar(&nodesflag, "nodes", "nodes.dmp", "nodes.dmp file of taxonomy")
+	flag.StringVar(&namesflag, "names", "names.dmp", "names.dmp file of taxonomy")
+	flag.StringVar(&bintaxflag, "bintax", "", "taxonomy in binary format (from tax2bin) [optional]")
+	flag.StringVar(&dictflag, "dict", "", "dict file of taxonomy")
+	flag.StringVar(&taxlevel, "levels", "family", "Desired taxonomical levels")
+	flag.StringVar(&gi2kegg, "gi2kegg", "", "genes_ncbi-gi.list file from kegg [optional]")
+	//	flag.StringVar(&kegg2pw, "kegg2pw", "", "genes_pathway.list file from kegg [optional]")  // For now, we leave out pathways... can be recovered later
+	flag.StringVar(&binkegg, "binkegg", "", "kegg in binary format (from kegg2bin) [optional]")
+	flag.BoolVar(&savememflag, "savemem", false, "save memory by keeping files in disk [optional]")
+	flag.BoolVar(&verflag, "version", false, "Print VERSION and exits")
+	flag.BoolVar(&helpflag, "help", false, "Print USAGE and exits")
+	flag.Parse()
+	//	if (kegg2gi != "" && kegg2pw != "") { // TODO: Treat kegg2gi and kegg2pw independently?
+	if gi2kegg != "" {
+		keggflag = true
+	} else {
+		keggflag = false
+	}
+	//	if (kegg2gi != "" && kegg2pw == "") || (kegg2gi == "" && kegg2pw != "") {
+	//		fmt.Fprintf(os.Stderr, "If KEGG analysis is required, both genes_ncbi-gi.list and genes_pathway.list files are required. Only one provided\n")
+	//		os.Exit(1)
+	//	}
+	if keggflag && savememflag && binkegg == "" {
+		fmt.Fprintf(os.Stderr, "In savemem mode you need to specify the kegg binary file. See the docs for details\n")
+		os.Exit(1)
+	}
+	if binkegg != "" {
+		fmt.Fprintf(os.Stderr, "Binary kegg file %s will be used for kegg assignment\n", binkegg)
+		keggIsBin = true
+	} else {
+		keggIsBin = false
+	}
+	if bintaxflag != "" {
+		fmt.Fprintf(os.Stderr, "Binary taxonomy file %s will be used for taxonomy tree construction\n", bintaxflag)
+		taxIsBin = true
+	} else {
+		taxIsBin = false
+	}
+	blastfile = flag.Arg(0)
+	if verflag {
+		fmt.Printf("blast2lca\nVERSION: %.3f\n\n", VERSION)
+		os.Exit(0)
+	}
+	if helpflag {
+		fmt.Printf("blast2lca\n")
+		flag.Usage()
+		os.Exit(0)
+	}
+
+	if blastfile == "" {
+		fmt.Printf("blast2lca\n")
+		flag.Usage()
+		fmt.Printf("\nA blast file is mandatory\n\n")
+		os.Exit(1)
+	}
+	runtime.GOMAXPROCS(cpuflag)
+}
+
+func main() {
+	opts := &taxonomy.InOpts{
+		Nodes:    nodesflag,
+		Names:    namesflag,
+		Dict:     dictflag,
+		Bintax:   bintaxflag,
+		TaxIsBin: taxIsBin,
+		Savemem:  savememflag,
+	}
+	levs := bytes.Split([]byte(taxlevel), []byte{':'})
+	taxDB, err := taxonomy.New(opts)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "ERROR : Impossible to get a valid Taxonomy: %s\n", err)
+		os.Exit(1)
+	}
+
+	// BLAST
+	blastf, eopen := os.OpenFile(blastfile, os.O_RDONLY, 0644)
+	if eopen != nil {
+		fmt.Fprintf(os.Stderr, "file %s doesn't exist\n", blastfile)
+		os.Exit(1)
+	}
+	defer blastf.Close()
+
+	blastbuf := bufio.NewReader(io.Reader(blastf))
+	var keggDB kegg.Mapper
+//	keggflag = false
+	if keggflag == true {
+		keggDB, err = kegg.Load(gi2kegg, binkegg, savememflag)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "I can't load the Kegg database: %s", err)
+			os.Exit(1)
+		}
+	}
+	queryChan := make(chan *blastm8.QueryRes, (cpuflag+1)) // TODO: Should we do this buffered?
+	//	doneChan := make(chan bool)                    // TODO: Should we do this buffered?
+	launched := 0
+	finished := make(chan bool) // TODO: Needed that much?? Profile... any performance impact?
+	//	go blastm8.Procfile(blastbuf, queryChan, doneChan, keggflag)
+	go blastm8.Procfile(blastbuf, queryChan, keggflag)
+
+	t1 := time.Nanoseconds()
+LOOP:
+	for {
+		if k, ok := <-queryChan; ok {
+			launched++
+			go func() {
+				taxids := make([]int, 0, 100)
+				bscLim := k.Best.GetBitsc() * bscLimFactor
+				for _, gibs := range k.Hits {
+					taxid, err := taxDB.TaxidFromGi(gibs.GetGI())
+					if err != nil {
+						continue
+					}
+					if gibs.GetBitsc() >= bscLim {
+						taxids = append(taxids, taxid)
+					} else {
+						break
+					}
+				}
+
+				lcaNode, err := taxDB.LCA(taxids...)
+				var atLevs [][]byte
+				if err != nil {
+					atLevs = make([][]byte, 1)
+					atLevs[0] = []byte("unknown")
+				} else {
+					atLevs = taxDB.AtLevels(lcaNode, levs...)
+				}
+
+				allLevs := bytes.Join(atLevs, []byte(";"))
+
+				keggIDs := ""
+				for _, hits := range k.BySubj {
+					hitBsLimit := hits.Best.GetBitsc() * bscLimFactor
+					for _, hit := range hits.Recs {
+						if hit.GetBitsc() < hitBsLimit {
+							break
+						}
+						kegg, err := keggDB.Gi2Kegg(hit.GetGI())
+						if err != nil {
+							fmt.Fprintf(os.Stderr, "ERROR: Got an error from kegg.Gi2Kegg: %s\n", err)
+						}
+						if kegg != nil {
+							keggIDs += fmt.Sprintf("%s;", kegg)
+							break
+						}
+					}
+				}
+				printf("%s\t%s\t%s\n", k.Query, allLevs, keggIDs)
+				finished <- true
+			}()
+		} else {
+			for i := 0; i < launched; i++ {
+				<-finished
+			}
+			break LOOP
+		}
+	}
+	t2 := time.Nanoseconds()
+	secs := float32(t2-t1)/1e9;
+	fmt.Fprintf(os.Stderr, "%d sequences analyzed in %.3f seconds (%d sequences per second)\n", launched, secs, int32(float32(launched) / secs))
+}
---
+++ tax2bin/tax2bin.go
@@ -0,0 +1,216 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"io"
+	"bytes"
+	"bufio"
+	"strings"
+	"strconv"
+	"gob"
+	"time"
+	"flag"
+)
+
+const VERSION = 0.01
+const sep = "\t|\t"
+
+var (
+	namesfile, nodesfile, outfile string
+	helpflag                      bool
+)
+
+type tids struct {
+	Taxid int
+	Name  string
+}
+
+type taxon struct {
+	Tids  *tids
+	Taxon string
+}
+
+type Taxnode struct {
+	This     taxon
+	ParentId int
+}
+
+type taxonomy map[int]*Taxnode
+
+func init() {
+	flag.StringVar(&nodesfile, "nodes", "nodes.dmp", "nodes.dmp file of taxonomy")
+	flag.StringVar(&namesfile, "names", "names.dmp", "names.dmp file of taxonomy")
+	flag.StringVar(&outfile, "out", "taxonomy.bin", "output file -- defaults to taxonomy.bin")
+
+	flag.BoolVar(&helpflag, "help", false, "Print usage and exits")
+	flag.Parse()
+	if helpflag {
+		fmt.Printf("tax2bin\nVERSiON: %.3f\n\n", VERSION)
+		flag.Usage()
+		os.Exit(0)
+	}
+}
+
+func readnames(b *bufio.Reader, namesch chan<- *tids) {
+	for {
+		line, err := b.ReadString('\n')
+		if err == os.EOF {
+			//			bch <- true
+			close(namesch)
+			return
+		}
+		if pos := strings.Index(line, "scientific name"); pos == -1 {
+			continue
+		}
+		newName := parsename([]byte(line)[0 : len(line)-3]) // ???
+		//		fmt.Println(*newName);
+		namesch <- newName
+	}
+	//	bch <- true
+	close(namesch)
+	return
+}
+
+func parsename(l []byte) *tids {
+	parts := bytes.Split(l, []byte(sep))
+	tid, _ := strconv.Atoi(fmt.Sprintf("%s", parts[0]))
+	newName := &tids{
+		tid,
+		fmt.Sprintf("%s", parts[1])}
+	return newName
+}
+
+func parsenode(l []byte) *Taxnode {
+	parts := bytes.SplitN(l, []byte(sep), 4)
+	tid, _ := strconv.Atoi(fmt.Sprintf("%s", parts[0]))
+	pid, _ := strconv.Atoi(fmt.Sprintf("%s", parts[1]))
+	//	fmt.Println("KKKK:",tid);
+	newRec := Taxnode{
+		taxon{
+			&tids{
+				tid,
+				""},
+			fmt.Sprintf("%s", parts[2])},
+		pid,
+	}
+	return &newRec
+}
+
+func newTaxonomy(b *bufio.Reader) taxonomy {
+	taxmap := make(map[int]*Taxnode)
+	for {
+		line, err := b.ReadString('\n')
+		if err == os.EOF {
+			return taxmap
+		}
+		newNode := parsenode([]byte(line)[0 : len(line)-3]) // ends with \t\|\n
+		//		fmt.Println("KK:",newNode.this.taxid);
+		taxmap[newNode.This.Tids.Taxid] = newNode
+	}
+	return taxmap
+}
+
+func New(nodes, names string) taxonomy {
+	nodesf, eopen := os.OpenFile(nodes, os.O_RDONLY, 0644)
+	if eopen != nil {
+		fmt.Fprintf(os.Stderr, "file doesn't exist %s\n", nodes)
+		os.Exit(1)
+	}
+	nodesbuf := bufio.NewReader(io.Reader(nodesf))
+
+	namesf, eopen := os.OpenFile(names, os.O_RDONLY, 0644)
+	if eopen != nil {
+		fmt.Fprintf(os.Stderr, "file doesn't exist %s\n", names)
+		os.Exit(1)
+	}
+	namesbuf := bufio.NewReader(io.Reader(namesf))
+
+	//	endch := make(chan bool)
+	namesch := make(chan *tids, 1000)
+	go readnames(namesbuf, namesch)
+
+	tax := newTaxonomy(nodesbuf)
+	//	<-endch
+
+	for {
+		n, ok := <-namesch
+		if !ok {
+			break
+		}
+		tax[n.Taxid].This.Tids = n
+	}
+
+	// 	for n := range namesch {
+	// 		tax[n.Taxid].This.Tids = n
+	// 	}
+
+	return tax
+}
+
+// Not used in this program -- We use it only to store the new binary version of taxonomy
+func (t taxonomy) Store(fname string) os.Error {
+	b := new(bytes.Buffer)
+	enc := gob.NewEncoder(b)
+	err := enc.Encode(t)
+	if err != nil {
+		return err
+	}
+
+	fh, eopen := os.OpenFile(fname, os.O_CREATE|os.O_WRONLY, 0666)
+	defer fh.Close()
+	if eopen != nil {
+		return eopen
+	}
+	_, e := fh.Write(b.Bytes())
+	if e != nil {
+		return e
+	}
+	//	fmt.Fprintf(os.Stderr, "%d bytes successfully written to file\n", n)
+	return nil
+}
+
+func Load(fname string) (taxonomy, os.Error) {
+	fh, err := os.Open(fname)
+	if err != nil {
+		return nil, err
+	}
+	t := make(taxonomy)
+	dec := gob.NewDecoder(fh)
+	err = dec.Decode(&t)
+	if err != nil {
+		return nil, err
+	}
+	return t, nil
+}
+
+func main() {
+	fmt.Fprintf(os.Stderr, "Building the taxonomy tree ... ")
+	s1 := time.Nanoseconds()
+	newtax := New(nodesfile, namesfile)
+	s2 := time.Nanoseconds()
+	if newtax == nil {
+		fmt.Fprintf(os.Stderr, "\n ERROR: the map is empty\n")
+		os.Exit(1)
+	}
+	fmt.Fprintf(os.Stderr, "Done (%.5f)\n", (float32(s2-s1))/1e9)
+	fmt.Fprintf(os.Stderr, "Writing tree into binary file ... ")
+	s1 = time.Nanoseconds()
+	e := newtax.Store(outfile)
+	s2 = time.Nanoseconds()
+	if e != nil {
+		fmt.Println(e)
+		os.Exit(1)
+	}
+	fmt.Fprintf(os.Stderr, "Done (%.5f)\n", (float32(s2-s1))/1e9)
+
+	// 	s1 = time.Nanoseconds()
+	// 	_,err := Load("file.bin")
+	// 	s2 = time.Nanoseconds()
+	// 	if err != nil {
+	// 		fmt.Println(err)
+	// 		os.Exit(1)
+	// 	}
+	// 	fmt.Printf("tree read from file in %.5f seconds\n", float32(s2-s1)/1e9)
+
+}
